f669c22cfefcd7c039496243000f2f90c19540d5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-20 11:42:46 +0200

    lesson 5 upd

--- lesson5/dictionaries.py
+++ lesson5/dictionaries.py
@@ -54,7 +54,7 @@ persons_dict = {
     person1['id']: person1,
     person2['id']: person2
 }
-pprint.pprint(persons_dict)
+# pprint.pprint(persons_dict)
 
 # my_list = [[123456789, "Valeria Aynbinder"],
 #            [987765234, "Ziv Attias"],
@@ -64,11 +64,11 @@ pprint.pprint(persons_dict)
 #     if elem[0] == 783465234:
 #         print(elem[1])
 
-empty = {}
-empty = dict()
-
-empty_list = []
-empty_list = list()
+# empty = {}
+# empty = dict()
+#
+# empty_list = []
+# empty_list = list()
 
 # Implement a function insert_persons that receives int
 # number n as argument, and asks the user to insert
@@ -94,25 +94,96 @@ empty_list = list()
 # print(insert_persons(2))
 
 
-def insert_persons(num: int) -> dict:
-    persons = {}
-    fields = ('id', 'first_name', 'phone')
-    # fields(('id', int), ('first_nanme', str))
-    fields = {'id': int,
-              'first_name': str}
-    for i in range(num):
-        person = {}
-        for field in fields:
-            person[field] = input(f"Insert {field}: ")
-        persons[person['id']] = person
-    return persons
-
-print(insert_persons(2))
+# def insert_persons(num: int) -> dict:
+#     persons = {}
+#     fields = ('id', 'first_name', 'phone')
+#     # fields(('id', int), ('first_nanme', str))
+#     fields = {'id': int,
+#               'first_name': str}
+#     for i in range(num):
+#         person = {}
+#         for field in fields:
+#             person[field] = input(f"Insert {field}: ")
+#         persons[person['id']] = person
+#     return persons
+#
+# print(insert_persons(2))
 # representing an object
 
 # using operator [] for read and assignment
+grades = {
+    'Ziv': [100, 100, 100],
+    'Valeria': [89, 70],
+    'Yael': [99, 98, 100]
+}
+grades['Noa'] = [100]
+# grades[3] = True
+# pprint.pprint(grades)
+grades['Noa'].append(99)
+# pprint.pprint(grades)
+
 
 # iterating over dictionary
+# for xxx in grades:
+#     print(xxx)
+#     print(grades[xxx])
 
+# for xxx in grades.keys():
+#     print(xxx)
+#
+# for xxx in grades.values():
+#     print(xxx)
+
+# for key, val in persons_dict.items():
+#     print(key)
+#     print(val)
+#     if type(val) == dict:
+#         for k, v in val.items():
+#             print(f"k={k}")
+#             print(f"v={v}")
+#
+# persons_dict = {
+#     333333333: {
+#         'first_name': 'Valeria',
+#         'last_name': 'Aynbinder',
+#         'year': 1987,
+#         'id': 333333333,
+#         'phone': '0546666666'
+#     },
+#     111111111: {
+#         'first_name': 'Ziv',
+#         'last_name': 'Attias',
+#         'year': 1998,
+#         'id': 111111111,
+#         'phone': '0546664455'
+#     }
+# }
+# for person_id, person_dict in persons_dict.items():
+#     # if 'first_name' in person_dict:
+#     #     print(person_dict['first_name'])
+#     print(person_dict.get('first_name'))
+#     print(person_dict['bbbb'])
+
+
+
+# print(grades.keys())
+# print(grades.values())
 # dictionary methods
 
+# for name, grades_list in grades.items():
+#     print(f"{name}: {sum(grades_list) / len(grades_list)}")
+
+# name = input("Insert name: ")
+# print(f"{name}: {grades.get(name)}")
+# print(f"{name}: {sum(grades.get(name, [])) / len(grades.get(name, []))}")
+# print(grades)
+
+# grades.pop('Ziv')
+# del grades['Ziv']
+# new_grades = {
+#     'Yuval': [90, 95],
+#     'Nehorai': [98, 97]
+# }
+# grades.update(new_grades)
+# grades.update(new_grades)
+# print(grades)
