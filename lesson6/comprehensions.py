118df6298d4efa5d08c33954c7bdfd8c41198e4f
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-23 20:15:33 +0200

    more updates

--- lesson6/comprehensions.py
+++ lesson6/comprehensions.py
@@ -1,37 +1,37 @@
-colors = ['white', 'black', 'grey', 'red', 'green']
-
-upper_colors = [c.upper() for c in colors]
-my_colors_dict = {c: c[0] for c in colors}
-
-colors = ['white', 'black', 'grey', 'red', 'green', '']
-my_colors_dict = {c: c[0] if len(c) else "" for c in colors}
-
-my_colors_dict = {}
-for c in colors: #""
-    if len(c) > 0:
-        my_colors_dict[c] = c[0]
-    else: # c = ""
-        my_colors_dict[c] = ""
-
-# d = {'a':1, 'b':2, 'c':3}
-# d.get('d')
-
-d2 = {3: "ewre", 4:"", 5:[], 6:{}, 7:None, 8:10}
-# ("", [], {}, None)
-
-for key, value in d2.items():
-    if value is None or len(value) == 0:
-        # value is empty
-
-temp = 20
-weather = 'hot' if temp > 25 else 'cool'
-
-upper_colors = []
-for c in colors:
-    upper_colors.append(c.upper())
-
-num2idx = {n: i for i, n in enumerate(nums)}
-
-num2idx = {}
-for i, n in enumerate(nums):
-    num2idx[n] = i
+# colors = ['white', 'black', 'grey', 'red', 'green']
+#
+# upper_colors = [c.upper() for c in colors]
+# my_colors_dict = {c: c[0] for c in colors}
+#
+# colors = ['white', 'black', 'grey', 'red', 'green', '']
+# my_colors_dict = {c: c[0] if len(c) else "" for c in colors}
+#
+# my_colors_dict = {}
+# for c in colors: #""
+#     if len(c) > 0:
+#         my_colors_dict[c] = c[0]
+#     else: # c = ""
+#         my_colors_dict[c] = ""
+#
+# # d = {'a':1, 'b':2, 'c':3}
+# # d.get('d')
+#
+# d2 = {3: "ewre", 4:"", 5:[], 6:{}, 7:None, 8:10}
+# # ("", [], {}, None)
+#
+# for key, value in d2.items():
+#     if value is None or len(value) == 0:
+#         # value is empty
+#
+# temp = 20
+# weather = 'hot' if temp > 25 else 'cool'
+#
+# upper_colors = []
+# for c in colors:
+#     upper_colors.append(c.upper())
+#
+# num2idx = {n: i for i, n in enumerate(nums)}
+#
+# num2idx = {}
+# for i, n in enumerate(nums):
+#     num2idx[n] = i
