438b064c946298a072be28468ac2fa374f7be982
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-27 20:41:04 +0200

    lesson 7 store

--- lesson7/gadget_store.py
+++ lesson7/gadget_store.py
@@ -14,6 +14,43 @@ class Customer:
     def __repr__(self):
         return f"<Customer> {self.name}"
 
+
+class Product:
+    def __init__(self, sku: str, category: str, brand: str,
+                 qty: float, price: float,
+                 model: str = None,
+                 warranty_months: int = None):
+        if price <= 0:
+            pass
+            # error
+            # return None
+        self.sku = sku
+        self.category = category
+        self.price = price
+        self.qty = qty
+
+    def update_qty(self, diff: float):
+        if diff + self.qty < 0:
+            # error
+            return None
+        self.qty += diff
+
+    def update_price(self, new_price):
+        if new_price <= 0:
+            # error
+            return None
+        self.price = new_price
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self, other):
+        return self.sku == other.sku
+
+
 # c = Customer('adv', 'sdfs', 'sdfs')
 # c.phone = '054-342342'
 
@@ -28,6 +65,8 @@ class Store:
     def __init__(self, store_name):
         self.store_name = store_name
         self.customers: dict[str: Customer] = {}
+        self.inventory: {str: Product} = {}
+        self.inventory_by_name: dict[str: Product] = {}
         # customers
         # inventory
         # order_product
@@ -38,11 +77,28 @@ class Store:
         new_customer = Customer(name, address, phone, email)
         self.customers[name] = new_customer
 
-    def add_product_to_inventory(self):
-        pass
+    def add_product_to_inventory(self, sku: str, category: str, brand: str,
+                                 qty: float, price: float,
+                                 model: str = None,
+                                 warranty_months: int = None):
+        new_product = Product(sku, category, brand,
+                              qty, price, model, warranty_months)
+        self.inventory[sku] = new_product
+        self.inventory_by_name[brand+model] = new_product
 
-    def add_qty(self):
-        pass
+    def add_qty(self, sku:str, qty: float):
+        self.inventory[sku].update_qty(qty)
+
+    def add_items(self, skus: list, quantities: list):
+        for sku, qty in zip(skus, quantities):
+            self.add_qty(sku, qty)
+
+    def get_products_by_brand(self, brand) -> list[Product]:
+        ret_val = list()
+        for product in self.inventory.values():
+            if product.brand == brand:
+                ret_val.append(product)
+        return ret_val
 
     def get_out_of_stock(self):
         pass
