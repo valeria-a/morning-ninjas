44b60d369202cd7af14760de9a7979bf8792f2b6
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-04 11:46:13 +0200

    lesson9

--- lesson9/private_attr.py
+++ lesson9/private_attr.py
@@ -5,16 +5,24 @@ class Car:
     def __init__(self, manufacturer: str, model: str, color: str,
                  fuel_consumption: float, fuel_tank_capacity: float):
         self.manufacturer: str = manufacturer
-        self.model: str = model
-        self.color: str = color
-        self.fuel_consumption = fuel_consumption
+        self.__model: str = model
+        self.__color: str = color
+        self._fuel_consumption = fuel_consumption
         self.fuel_tank_capacity = fuel_tank_capacity
         self.__km: int = 0
         self.fuel: float = 0
 
-    def __str__(self):
-        return f"{self.manufacturer} | Model: {self.model} | Year: {self.year}"
+    def get_model(self):
+        return self.__model
 
+    def get_color(self):
+        return self.__color
+
+    def set_color(self, new_color):
+        self.__color = new_color
+
+    def __str__(self):
+        return f"{self.manufacturer} | Model: {self.__model} |  km: {self.__km} | fuel: {self.fuel}"
 
     def fill_tank(self, amnt: float) -> bool:
         if amnt <= 0:
@@ -37,4 +45,12 @@ class Car:
 if __name__ == '__main__':
     car_mazda: Car = Car('Mazda', '3', 'white', 20, 60)
     car_toyota = Car('Toyota', 'Yaris', 'red', 25, 45)
-    print(car_toyota.__dir__())
+    # ret_val = car_mazda.drive(30)
+    # print(ret_val)
+    print(car_mazda.__dir__())
+    print(car_mazda._Car__km)
+    # car_mazda._Car__km = 30
+    # car_mazda.km = 60
+    print(car_mazda.get_color())
+    print(car_mazda._fuel_consumption)
+    # print(car_mazda.__km)
---
+++ lesson9/rav_kav.py
@@ -0,0 +1,55 @@
+import datetime
+
+
+class RavKav:
+
+    rides = {'short': {'range': (0, 15), 'price': 5.5},
+             'medium': {'range': (16, 40), 'price': 12},
+             'long': {'range': (40, 1000), 'price': 40},
+             }
+
+    def __init__(self, holder_id, holder_name):
+        self.__holder_id = holder_id
+        self.__holder_name = holder_name
+        self.__balance = 0
+        # self.__log = {
+        #     'log_dates': {},
+        #     'log_types': {}
+        # }
+        self.__log_date = {}
+        self.__log_types = {}
+
+    def get_holder_id(self):
+        return self.__holder_id
+
+    def get_holder_name(self):
+        return self.__holder_name
+
+    def topup(self, amnt):
+        if amnt > 0:
+            self.__balance += amnt
+
+    @staticmethod
+    def _km2type(km) -> str:
+        for ride_type, ride_details in RavKav.rides.items():
+            if ride_details['range'][0] <= km <= ride_details['range'][1]:
+                return ride_type
+
+    def ride(self, km, date: datetime.date):
+        pass
+
+
+# d1 = datetime.datetime.now()
+d2 = input("Insert a date dd-mm-yyyy: ")
+date2 = datetime.datetime.strptime(d2, "%d-%m-%Y")
+print(date2.date())
+
+
+
+
+
+
+
+
+
+
