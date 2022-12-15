d42e897b2251926f771e0ad59d9ed3c98282dca3
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-15 15:50:42 +0200

    lesson 11 jb

---
+++ lesson12/lesson_code.py
@@ -0,0 +1,10 @@
+# review of OOP
+
+# encapsulation
+# polymorphism
+# inheritance
+# abstraction
+
+# github from pycharm
+# debugging - debug a code to find a bug
+# how to solve problems in code using debugger and expression evaluator
