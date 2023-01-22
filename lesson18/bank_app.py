53a35e410e9367206a839d76e809799bf7f87e71
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-01-22 20:16:07 +0200

    more solved tutorials

--- lesson18/bank_app.py
+++ lesson18/bank_app.py
@@ -17,7 +17,7 @@ conn = psycopg2.connect(
     password="postgres")
 
 
-@app.route("api/v1/customers/<int:customer_id>", methods=['GET'])
+@app.route("/api/v1/customers/<int:customer_id>", methods=['GET'])
 def get_customer(customer_id):
     print(f"called /customers/customer_id/{customer_id}")
     with conn:
@@ -33,22 +33,22 @@ def get_customer(customer_id):
                     'address': result[3]
                 }
                 # option I
-                response = app.response_class(
-                    response=json.dumps(ret_data),
-                    status=200,
-                    mimetype='application/json'
-                )
-                return response
+                # response = app.response_class(
+                #     response=json.dumps(ret_data),
+                #     status=200,
+                #     mimetype='application/json'
+                # )
+                # return response
 
                 # option II
-                # return jsonify(ret_data)
+                return jsonify(ret_data)
             else:
                 return app.response_class(
                     status=404
                 )
 
 
-@app.route("/customers/<int:customer_id>", methods=['PUT'])
+@app.route("/api/v1/customers/<int:customer_id>", methods=['PUT'])
 def update_customer(customer_id):
     new_data = request.form
     updates_str_list = []
@@ -57,13 +57,13 @@ def update_customer(customer_id):
     sql = f"UPDATE customers SET {','.join(updates_str_list)} WHERE id=%s"
     with conn:
         with conn.cursor() as cur:
-            cur.execute(sql, tuple(new_data.keys()) + tuple([customer_id]))
+            cur.execute(sql, tuple(new_data.values()) + tuple([customer_id]))
             if cur.rowcount == 1:
                 # update succeeded
                 return app.response_class(status=200)
     return app.response_class(status=500)
 
-@app.route("/customers", methods=['GET'])
+@app.route("/api/v1/customers", methods=['GET'])
 def get_customers():
     return 'get customers'
 
