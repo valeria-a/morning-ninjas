0d7d7eabe4f94a4b7b27dffd45c49654212d5451
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-05-07 09:45:05 +0300

    lesson 44

--- lesson44/patterns/singletone/singletone3.py
+++ lesson44/patterns/singletone/singletone3.py
@@ -1,20 +1,17 @@
+from threading import Lock
+
+
 class Singleton:
     _instance = None
+    _lock = Lock()
 
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            # cls._instance.cache = {}
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
         return cls._instance
-
-    def __init__(self):
-        if not self.__getattribute__('cache'):
-            self.cache = {}
-
-
-    def add_to_cache(self, k, v):
-        self.cache[k] = v
 
 
 if __name__ == '__main__':
